b866e89f24957f42ef6511bf9212685f264e4ac6
eros.montin <eros.montin@gmail.com>
2023-12-13 18:53:54 -0500

    labelmappable to imaginable

--- pyable_eros_montin/dev.py
+++ pyable_eros_montin/dev.py
@@ -9,47 +9,6 @@ except:
 
 import numpy as np
 
-class LabelMapable(Imaginable):
-    def __init__(self, filename=None, image=None, verbose=False,labelsvalues=None):
-        super().__init__(filename, image, verbose)
-        self.dfltInterpolator=sitk.sitkNearestNeighbor
-        self.dfltuseNearestNeighborExtrapolator=True
-        self.labelsvalues=labelsvalues
-        self.ROIS=[]
-        if self.labelsvalues is None:
-            self.labelsvalues=self.getImageUniqueValues(exclude=[0])
-
-        for v in self.labelsvalues:
-            self.ROIS.append(Roiable(filename,image,verbose,roivalue=v))
-        
-
-    def removeSmallObj(self,voxel_threshold=50,connectivity=26):
-        for r in self.ROIS:
-            r.removeSmallObj(voxel_threshold,connectivity)
-        self.mergeLabels()
-        return self
-
-    def removeHoles(self,voxel_threshold=50,connectivity=26):
-        for r in self.ROIS:
-            r.removeHoles(voxel_threshold,connectivity)
-        # self.mergeLabels()
-        return self
-    def keepBiggestObj(self,connectivity=26):
-        for r in self.ROIS:
-            r.keepBiggestObj(connectivity)
-        # self.mergeLabels()
-        return self
-
-    def mergeLabels(self):
-        for rd,v in zip(self.ROIS,self.labelsvalues):
-            print(v)
-            O=rd.getImageAsNumpy()
-            try:
-                LABELMAP[np.where(O==1)]=v    
-            except NameError:
-                LABELMAP=O
-        self.setImageFromNumpy(LABELMAP,refimage=super().getImage())
-        return self
     
 
 
